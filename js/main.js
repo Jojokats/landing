ea8b2704b673488b1198dd2f407ee937c0369846
Joanna <morningstarjk@hotmail.com>
2017-06-04 13:55:27 -0400

    added images and finished coding the index.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+$(document).ready(function() {
+  $('section#screenshots a').on('click', function() {
+    $('div#modal img').attr('src', $(this).attr('data-image-url') );
+
+  });
+});
+
+// this - refers to $('section#screenshots a')
+//that executed the above funciton
