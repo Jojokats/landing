85bd69a9db80b4a93b1cb590d9cafe44a76c5833
Joanna <morningstarjk@hotmail.com>
2017-06-05 13:13:34 -0400

    Finished adding the scroll navbar in javaScript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 
 $(document).ready(function() {
+
   $('section#screenshots a').on('click', function() {
     $('div#modal img').attr('src', $(this).attr('data-image-url') );
-
   });
-});
+  // this - refers to $('section#screenshots a') that executed the above funciton
 
-// this - refers to $('section#screenshots a')
-//that executed the above funciton
+  var nav = $('.navbar-fixed-top');
+  var distance = $('.navbar-fixed-top').offset();
+
+  // console.log(distance);
+
+  if (distance.top >= 300) {
+    nav.addClass('effect');
+  }
+
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >=300) {
+      nav.addClass('effect');
+    } else {
+      nav.removeClass('effect');
+    }
+  });
+
+});
